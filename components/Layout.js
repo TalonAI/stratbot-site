2b7f04725b380142386f342bb6ae8744f947c012
TalonAI <paulmaggio56@gmail.com>
2025-04-15 16:43:07 -0400

    layout fix with v0

--- components/Layout.js
+++ components/Layout.js
@@ -10,14 +10,32 @@ export default function Layout({ children }) {
     <div className="min-h-screen flex flex-col">
       <nav className="bg-white shadow-md">
         <div className="max-w-7xl mx-auto px-6 py-4">
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between w-full">
             {/* Logo/Brand - Left justified */}
-            <Link href="/" legacyBehavior>
-              <a className="text-2xl font-extrabold text-gray-900 hover:text-blue-600">StratBot</a>
-            </Link>
+            <div className="flex-shrink-0">
+              <Link href="/" legacyBehavior>
+                <a className="text-2xl font-extrabold text-gray-900 hover:text-blue-600">StratBot</a>
+              </Link>
+            </div>
+
+            {/* Spacer to push navigation to the right */}
+            <div className="flex-grow"></div>
+
+            {/* Desktop Navigation - Right justified */}
+            <div className="hidden md:flex md:items-center text-lg">
+              <Link href="/team" legacyBehavior>
+                <a className="text-gray-700 hover:text-blue-600 ml-12">Team</a>
+              </Link>
+              <Link href="/contact" legacyBehavior>
+                <a className="text-gray-700 hover:text-blue-600 ml-12">Contact</a>
+              </Link>
+              <Link href="/blog" legacyBehavior>
+                <a className="text-gray-700 hover:text-blue-600 ml-12">Blog</a>
+              </Link>
+            </div>
 
             {/* Mobile menu button */}
-            <div className="md:hidden">
+            <div className="md:hidden ml-4">
               <button
                 onClick={() => setIsMenuOpen(!isMenuOpen)}
                 className="text-gray-500 hover:text-gray-700 focus:outline-none"
@@ -30,19 +48,6 @@ export default function Layout({ children }) {
                   )}
                 </svg>
               </button>
-            </div>
-
-            {/* Desktop Navigation - Right justified */}
-            <div className="hidden md:flex md:items-center md:space-x-12 text-lg">
-              <Link href="/team" legacyBehavior>
-                <a className="text-gray-700 hover:text-blue-600">Team</a>
-              </Link>
-              <Link href="/contact" legacyBehavior>
-                <a className="text-gray-700 hover:text-blue-600">Contact</a>
-              </Link>
-              <Link href="/blog" legacyBehavior>
-                <a className="text-gray-700 hover:text-blue-600">Blog</a>
-              </Link>
             </div>
           </div>
 
