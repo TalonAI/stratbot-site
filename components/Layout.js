d0d67d36b4229ff88eb896e5a58996a5b1d4990b
TalonAI <paulmaggio56@gmail.com>
2025-04-20 13:19:03 -0400

    Update layout styling and sidebar

--- components/Layout.js
+++ components/Layout.js
@@ -6,29 +6,29 @@ export default function Layout({ children }) {
     <div className="min-h-screen flex flex-col bg-[#fdfaf3] text-gray-800">
       <nav className="bg-white shadow-sm">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          {/* Fresh nav: flex justify-around for equal spacing */}
+          {/* Nav with icon-only leading margin */}
           <div className="flex justify-around items-center h-16">
             <Link href="/" className="text-blue-700 font-bold text-xl hover:text-blue-800">
               StratBot
             </Link>
 
             <Link href="/contact" className="flex items-center text-gray-700 hover:text-blue-700">
-              <Mail className="w-5 h-5 mr-1" />
+              <Mail className="w-5 h-5 ml-8" />
               Contact
             </Link>
 
             <Link href="/team" className="flex items-center text-gray-700 hover:text-blue-700">
-              <Users className="w-5 h-5 mr-1" />
+              <Users className="w-5 h-5 ml-8" />
               Team
             </Link>
 
             <Link href="/blog" className="flex items-center text-gray-700 hover:text-blue-700">
-              <FileText className="w-5 h-5 mr-1" />
+              <FileText className="w-5 h-5 ml-8" />
               Blog
             </Link>
 
             <Link href="/book" className="flex items-center text-gray-700 hover:text-blue-700">
-              <Calendar className="w-5 h-5 mr-1" />
+              <Calendar className="w-5 h-5 ml-8" />
               Book a Call
             </Link>
           </div>
